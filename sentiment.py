4af91d0f147fe90bb02ac598cc8e5a8b2259e1c6
cponeill <caseyoneill78@hotmail.com>
2013-06-13 10:38:37 -0600

    Update sentiment.py

--- sentiment.py
+++ sentiment.py
@@ -1,27 +1,8 @@
 import sys
-import oauth2 as oauth
-import urllib2 as urllib
-from urllib2 import urlopen
-import json
+# import oauth2 as oauth
+# from urllib2 import urlopen
+# import json
 
-access_token_key = " " # Enter your Twitter access token
-access_token_secret = " " # Enter your Twitter access token secret
-
-consumer_key = " " # Enter your consumer key
-consumer_secret = " " # Enter your consumer secret
-
-_debug = 0
-
-oauth_token    = oauth.Token(key=access_token_key, secret=access_token_secret)
-oauth_consumer = oauth.Consumer(key=consumer_key, secret=consumer_secret)
-
-signature_method_hmac_sha1 = oauth.SignatureMethod_HMAC_SHA1()
-
-http_method = "GET"
-
-
-http_handler  = urllib.HTTPHandler(debuglevel=_debug)
-https_handler = urllib.HTTPSHandler(debuglevel=_debug)
 
 file = open(' ') # Enter the text file with the list of words and ratings here
 scores = {}
